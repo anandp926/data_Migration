30a45be6ba27f74bf85e71dd7a2a136b3877df0f
anandp926 <sanand926@gmail.com>
2019-03-03 01:09:24 +0530

    created tables component

---
+++ src/pages/tables/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Tables extends Component {
+    render() {
+        return(
+            <div className="container">Hi i am table page</div>
+        )
+    }
+}
+
+export default Tables
