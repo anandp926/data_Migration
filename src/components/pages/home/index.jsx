fb91157188c43aef52901bb2ead7735c7725c9cf
anandp926 <sanand926@gmail.com>
2019-03-02 15:15:02 +0530

    created home page

---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return(
+            <div>Hi I'm home page</div>
+        )
+    }
+}
+
+export default Home
