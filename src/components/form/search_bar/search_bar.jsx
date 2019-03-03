690912b9633525419c1f8c066f08bd3d2498f9aa
anandp926 <sanand926@gmail.com>
2019-03-03 14:05:12 +0530

    created search-bar component

---
+++ src/components/form/search_bar/search_bar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+const styles = {
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: '10px'
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  }
+};
+
+function SearchBar(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root} elevation={1}>
+      <InputBase className={classes.input} placeholder="Search Table Name" />
+      <IconButton className={classes.iconButton} aria-label="Search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
+
+SearchBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SearchBar);
